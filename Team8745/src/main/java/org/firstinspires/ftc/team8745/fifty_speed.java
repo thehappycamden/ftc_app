6dc8b9ef541ceffafdcd390134ba86d4515f5a51
rose <crashtest64@micahsolomon.com>
2017-01-02 08:27:09 -0800

    The gamepads are giving me errors, but I got this ready for beacon modifications, shooter, ball pickup, etc.!

---
+++ Team8745/src/main/java/org/firstinspires/ftc/team8745/fifty_speed.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.team8745;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+/**
+ * Created by rose on 1/2/17.
+ */
+@TeleOp(name="8K 50% Speed")
+
+
+public class fifty_speed {
+
+    DcMotor left_b;
+    DcMotor left_f;
+    DcMotor right_b;
+    DcMotor right_f;
+    public void init() {
+        //right
+        right_b = hardwareMap.dcMotor.get("motor-rightBACK");
+        right_f = hardwareMap.dcMotor.get("motor-right");
+
+        //left
+        left_b = hardwareMap.dcMotor.get("motor-leftBACK");
+        left_f = hardwareMap.dcMotor.get("motor-left");
+
+        //reverse mode
+        left_b.setDirection(DcMotorSimple.Direction.REVERSE);
+        left_f.setDirection(DcMotorSimple.Direction.REVERSE);}
+
+    boolean wasDown = false;
+
+    boolean slowMode = false;
+
+    @Override
+    public void loop() {
+        float r_stick = gamepad1.right_stick_y;
+        float l_stick = gamepad1.left_stick_y;
+        float r_slow = gamepad1.right_stick_y*.5;
+        float l_slow = gamepad1.left_stick_y*.5;
+
+        boolean down = gamepad1.dpad_down;
+
+        boolean down_click = false;
+
+
+
+        down_click = wasDown&&!down;
+
+        wasDown = down;
+
+        if (down_click){
+            slowMode = !slowMode;
+        }
+
+        if (!slowMode){
+            left_b.setPower(l_stick);
+            left_f.setPower(l_stick);
+            right_b.setPower(r_stick);
+            right_f.setPower(r_stick);
+        }
+        else {
+            right_f.setPower(r_slow);
+            right_b.setPower(r_slow);
+            left_f.setPower(l_slow);
+            left_b.setPower(l_slow);
+
+        }
+
+    }
+}
